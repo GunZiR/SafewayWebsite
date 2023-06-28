29154109577197f1cbf82f65dec46951f1b4176e
Your Name <you@example.com>
2023-06-28 17:12:21 +0700

    add motto typing effect

--- static/JavaScripts/base.js
+++ static/JavaScripts/base.js
@@ -14,42 +14,31 @@ document.addEventListener("DOMContentLoaded", function(){
         } 
     });
 });
-
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
+var TxtType = function(el, fullTxt, period) {
+    this.fullTxt = fullTxt[0];
     this.el = el;
-    this.loopNum = 0;
     this.period = parseInt(period, 10) || 2000;
     this.txt = '';
     this.tick();
-    this.isDeleting = false;
+    this.delta = 500;
+    this.total = 0;
 };
 
 TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
+    this.txt = this.fullTxt.substring(0, this.txt.length + 1);
     this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-    
+
     var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
+    var delta = this.delta;
+    
+    
+    if (this.txt === this.fullTxt) {
         delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
+        console.log('last: ');
+        this.txt = '';
     }
+    this.total += delta;
+    console.log('total: ' + this.total);
 
     setTimeout(function() {
         that.tick();
@@ -66,5 +55,4 @@ window.onload = function() {
             new TxtType(elements[i], JSON.parse(toRotate), period);
         }
     }
-    // INJECT CSS
 };
