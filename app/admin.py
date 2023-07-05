8ab27c6eddaed8242452cc3c74f87ef07ed2b733
Your Name <you@example.com>
2023-07-05 16:56:42 +0700

    add tail lift & tail gate model

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Truck
+from .models.tail_gate_tail_lift_model import TailGateTailLiftType, TailGateTailLiftSize, TailGateTailLiftBox, TailGateTailLiftProduct 
 
 # Register your models here.
-admin.site.register(Truck)
+admin.site.register(TailGateTailLiftType)
+admin.site.register(TailGateTailLiftSize)
+admin.site.register(TailGateTailLiftBox)
+admin.site.register(TailGateTailLiftProduct)
