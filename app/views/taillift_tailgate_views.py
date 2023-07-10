927a4a028659fc169c9d429dc5c4759ac8ac6846
Your Name <you@example.com>
2023-07-10 09:41:08 +0700

    complete database model and view

--- app/views/taillift_tailgate_views.py
+++ app/views/taillift_tailgate_views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
-# from ..models import TailGateTailLift
+from ..models import TailGateTailLift
 
 def type(request):
-    return render(request, 'taillift-tailgate\\type.html')
+    types = TailGateTailLift.objects.all()
+    content = {'types': types}
+    return render(request, 'taillift-tailgate\\type.html', content)
 
 def size(request, type):
-    return render(request, 'taillift-tailgate\\size.html')
+    content = {'header': type.split(' '), 'type': type}
+    node = get_object_or_404(TailGateTailLift, name=type)
+    sizes = node.get_children()
+    content['sizes'] = sizes
+    return render(request, 'taillift-tailgate\\size.html', content)
 
 def box(request, type, size):
-    return render(request, 'taillift-tailgate\\box.html')
+    content = {'header': size.split(' '), 'type': type, 'size': size}
+    node = get_object_or_404(TailGateTailLift, name=size)
+    boxes = node.get_children()
+    content['boxes'] = boxes
+    return render(request, 'taillift-tailgate\\box.html', content)
 
 def product(request, type, size, box):
-    return render(request, 'taillift-tailgate\\product.html')
+    content = {'header': box.split(' '), 'type': type, 'size': size, 'box': box}
+    node = get_object_or_404(TailGateTailLift, name=box)
+    products = node.get_children()
+    content['products'] = products
+    return render(request, 'taillift-tailgate\\product.html', content)
